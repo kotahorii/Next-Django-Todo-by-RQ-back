aa0cf812fe27d38bff7138ce9ba8d1a6411f6510
kotahorii <khorii.micron@gmail.com>
2021-11-02 13:43:54 +0900

    views.py updated

--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, viewsets
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from . import serializers
 from .models import Tag, Task
 
@@ -23,6 +22,12 @@ class TagListView(generics.ListAPIView):
     permission_classes = (AllowAny,)
 
 
+class TagRetrieveView(generics.RetrieveAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+    permission_classes = (AllowAny,)
+
+
 class TaskListView(generics.ListAPIView):
     queryset = Task.objects.all()
     serializer_class = serializers.TaskSerializer
@@ -35,16 +40,9 @@ class TaskRetrieveView(generics.RetrieveAPIView):
     permission_classes = (AllowAny,)
 
 
-class TaskCreateView(generics.CreateAPIView):
-    serializer_class = serializers.TagSerializer
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = serializers.TaskSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-
-class TaskUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Task.objects.all()
-
-    def retrieve(self, request, *args, **kwargs):
-        response = {'message': 'GET method is not allowed'}
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
